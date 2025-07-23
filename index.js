94203a526cd8e7524a3f5a2be90fe72380d0bf4e
Pankaj <pankaj.kushwaha@sparkeighteen.com>
2025-07-23 18:06:03 +0530

    Rectified index.js and package.json

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-
-import express, { urlencoded } from 'express'
+import express from 'express'
 const app=express()
+
 app.use(express.json)
-app.use(urlencoded)
 const port=4000
 
 app.listen(port, (req,res)=>{
