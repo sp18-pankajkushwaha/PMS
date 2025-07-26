bc1382bec9f8fb53afaf84c4139ff1b0eead1cfc
Pankaj <pankaj.kushwaha@sparkeighteen.com>
2025-07-26 11:25:23 +0530

    removed mongodb connection code

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 import express from 'express'
-import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-
 
 dotenv.config()
 const app=express()
 app.use(express.json())
 const PORT=process.env.PORT
 
-const MONGO_URI=process.env.MONGO_URI
-
-mongoose.connect(MONGO_URI)
-        .then(()=>console.log('Connected to MongoDB'))
-        .catch((err)=>console.log(err)) 
 
 app.listen(PORT, (req,res)=>{
     console.log(`Connected to Port ${PORT}`)
